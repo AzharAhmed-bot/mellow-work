91cd2e2094d6e202d42ff1a743403ed382123d6f
+ <+>
2023-11-21 20:32:54 +0300

    🧱 worked on datatypes

--- HelloWorld/src/CrashCourse.java
+++ HelloWorld/src/CrashCourse.java
@@ -1,3 +1,13 @@
 public class CrashCourse {
+  public static void main(String a[]){
+    boolean status=true;
+    byte by=127;// Literals are the values assiged to the variable
+    short sh=32767;
+    int in=2147483647;
+    long lo=9223372036854775807L;// Must put an L at the end of the number
+
     
+    System.out.println(status);
+
+  }
 }
