6e52bdfa6e42190c7a3f815ec218c82b3a34f3eb
+ <+>
2023-11-21 20:38:59 +0300

    🧐 exploring data conversion next

--- HelloWorld/src/CrashCourse.java
+++ HelloWorld/src/CrashCourse.java
@@ -1,13 +1,21 @@
 public class CrashCourse {
   public static void main(String a[]){
-    boolean status=true;
-    byte by=127;// Literals are the values assiged to the variable
-    short sh=32767;
-    int in=2147483647;
-    long lo=9223372036854775807L;// Must put an L at the end of the number
+    // boolean status=true;
+    // Literals are the values assiged to the variable
+    // byte by=127;
+    // short sh=32767;
+    // int in=2147483647;
+    // long lo=9223372036854775807L;// Must put an L at the end of the number
 
-    
-    System.out.println(status);
+    //It supports binary and hexadecimal 
+    // int hex_code=0x78;  
+    // int num=10_000; //works like a , the _(underscore) but it will be printed as 10000
+
+    //String literal
+    char c='a';
+    //You can increment values  in java it will print 'b'
+    c++;
+    System.out.println(c);
 
   }
 }
