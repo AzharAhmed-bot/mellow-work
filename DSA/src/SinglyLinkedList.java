10f3c02271ad84225ce3a9928c341a5132b219e1
azhar <azharahmedtakoy4444@gmail.com>
2024-04-24 16:51:41 +0300

    commented the code

--- DSA/src/SinglyLinkedList.java
+++ DSA/src/SinglyLinkedList.java
@@ -1,20 +1,35 @@
+// Visit the node.java to check out the node for the linked list
+
+
 public class SinglyLinkedList {
     Node head;
     public SinglyLinkedList(int head){
         this.head = new Node(head);
     }
 
+    // Create a new node with the given data and add it to the beginning of the list
+    // 1. Create a new node with the given data
+    // 2. Set the new node's next pointer to the current head
+    // 3. Set the head to the new node
     public void addFirst(int data){
         Node newNode=new Node(data);
         newNode.next=head;
         head= newNode;
     }
+
+    // Remove the first node from the list
+    // 1. If the list is not empty, set the head to the next node
     public void removeFirst(){
         if(head!=null){
             head=head.next;
         }
 
     }
+
+    // Add a new node with the given data to the end of the list
+    // 1. If the list is empty, add the node to the beginning
+    // 2. Traverse the list to find the last node
+    // 3. Set the last node's next pointer to the new node
     public void addLast(int data){
         Node current=head;
         if(current==null){
@@ -26,6 +41,12 @@ public class SinglyLinkedList {
         }
         current.next=new Node(data);
     }
+
+    // Remove the last node from the list
+    // 1. If the list is empty, do nothing
+    // 2. If the list has only one node, set the head to null
+    // 3. Traverse the list to find the second-to-last node
+    // 4. Set the second-to-last node's next pointer to null
     public void removeLast(){
         Node current=head;
         if(current==null){
@@ -40,6 +61,10 @@ public class SinglyLinkedList {
         current.next=null;
     }
 
+    // Delete the first occurrence of a node with the given key
+    // 1. Traverse the list to find the node with the given key
+    // 2. If found, update the previous node's next pointer to skip the current node
+    // 3. Return true if the node was found and deleted, false otherwise
     public boolean delete(int key){
         Node current=head;
         Node previous=null;
@@ -54,6 +79,9 @@ public class SinglyLinkedList {
         return false;
     }
 
+    // Print the contents of the list
+    // 1. Traverse the list and print each node's data
+    // 2. Print "null" at the end to indicate the end of the list
     public void printList() {
         Node current = head;
         while (current!= null) {
