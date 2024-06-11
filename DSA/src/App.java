ab873cd53809d660300911e2d625f93dae18a815
azhar <azharahmedtakoy4444@gmail.com>
2024-06-11 16:18:47 +0300

    revision

--- DSA/src/App.java
+++ DSA/src/App.java
@@ -9,7 +9,7 @@ public class App {
         return memo[n];
     }
     
-    public static int[] fib(int limit) {
+    public static int[] fib_up_to(int limit) {
         int[] result = new int[limit+1];
         result[0] = 0;
         result[1] = 1;
@@ -17,11 +17,25 @@ public class App {
         return result;
     }
 
-    
+    public static int gcd(int a, int b) {
+        // Ensure a is always non-negative
+        a = Math.abs(a);
+        
+      
+        if (b == 0) {
+          return a;
+        }
+      
+        
+        return gcd(b, a % b);
+      }
+      
+      
 
     public static void main(String[] args) throws Exception {
-        int[] fibSeries=fib(20);
+        int[] fibSeries=fib_up_to(10);
         System.out.println(Arrays.toString(fibSeries));
+        System.out.println(gcd(12, 513));
       
     }
 }
--- DSA/src/BST.java
+++ DSA/src/BST.java
@@ -1,3 +1,5 @@
+import javax.xml.crypto.Data;
+
 ;
 
 public class BST {
@@ -43,6 +45,91 @@ public class BST {
         }
     }
 
+    void delete(int data){
+        if(root==null){
+            System.out.println("The tree the empty");
+        }
+        else if(search(data)==false){
+            System.out.println("The key is not present in the tree");
+        }
+        else{
+            root=deleteRec(root,data);
+            System.out.println(data+" is deleted from the tree");
+        }
+    }
+
+    BSTNode deleteRec(BSTNode node, int data){
+        if(node==null){
+            return node;
+        }
+        // If data is less than the node on the right, we go on recursion on the left
+        if(data < node.key){
+            node.left=deleteRec(node.left,data);
+        }
+        // Vice versa
+        else if(data > node.key){
+            node.right=deleteRec(node.right,data);
+        }
+        else{
+            // If we reach the leaf node
+            if(node.left==null && node.right==null){
+                node=null;
+            }
+            // If it has a node but only one node:
+            // The node we delete is on the right
+            else if(node.left==null){
+                BSTNode temp=node;
+                node=node.right;
+                temp=null;
+            }
+            else if(node.right==null){
+                BSTNode temp=node;
+                node=node.left;
+                temp=null;
+            }
+            // If the node has 2 children, find the inorder successor (the smallest node in the right subtree) 
+            else{
+                BSTNode temp = minValueNode(node.right);
+                node.key = temp.key;
+                node.right = deleteRec(node.right, temp.key);
+
+            }
+        }
+        return node;
+    }
+
+
+    
+    BSTNode minValueNode(BSTNode node) {
+        BSTNode current = node;
+
+        // Loop down to find the leftmost node
+        while (current.left!= null) {
+            current = current.left;
+        }
+
+        // Return the leftmost node
+        return current;
+    }
+
+    public int countNodes(){
+        return countNodesRec(root);
+    }
+
+    public int countNodesRec(BSTNode root){
+        if(root==null){
+            return 0;
+        }
+        else{
+            int num=1;
+            num+=countNodesRec(root.right);
+            num+=countNodesRec(root.left);
+            return num;
+        }
+    }
+
+
+
     void printTree() {
         printTreeRec(root, 0, "");
     }
@@ -69,10 +156,14 @@ public class BST {
         System.out.println("Tree before search:");
         myTree.printTree();
         
-        myTree.search(20);
+        // myTree.search(20);
         
-        System.out.println("\nTree after search:");
+        System.out.println("\nTree after delete:");
+        // myTree.printTree();
+        myTree.delete(20);
+        myTree.delete(80);
         myTree.printTree();
+        System.out.println(myTree.countNodes());
     }
 }
 
